1a9d860b3d71550c69f1b3499914f1cd0429c6bb
lmhgithi <lmhfox@foxmail.com>
2020-08-10 18:14:16 +0800

    [liminghao] finish 3-object

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,13 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const expected = {
+    serialNumber: '12345',
+    type: 'accessory',
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  };
+
+  return Object.assign(source, expected);
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,5 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const values = Object.values(source);
+  return values.reduce((pre, cur) => Number(pre) + Number(cur));
 }
