f52122a21b41a98d30e6166261112f5e36c7b292
lmhgithi <lmhfox@foxmail.com>
2020-08-10 15:24:52 +0800

    [liminghao] finish 1-string

--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -6,5 +6,6 @@ function getScore(id) {
 }
 
 export default function getScoreOutput(person) {
+  return `你好，${person.lastName}${person.firstName}！你的考试成绩为${getScore(person.id)}分`;
   // TODO 8: 在这里写实现代码
 }
