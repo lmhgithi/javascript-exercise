d29578e80596f112a65ed4766494d5c19e0e7015
lmhgithi <lmhfox@foxmail.com>
2020-08-10 19:11:50 +0800

    [liminghao] finish 5-async

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,19 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
+  xhr.open('GET', url);
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      successCallback();
+    } else {
+      errorCallback();
+    }
+  };
+  xhr.error = () => {
+    errorCallback();
+  };
 
+  xhr.send();
   // end -->
 }
 
--- 5-async/2-promise/index.js
+++ 5-async/2-promise/index.js
@@ -3,7 +3,18 @@ function fetchData(url) {
     const xhr = new XMLHttpRequest();
     // <-- start
     // TODO 22: 通过Promise实现异步请求
-
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve();
+      } else {
+        reject();
+      }
+    };
+    xhr.error = () => {
+      reject();
+    };
+    xhr.send();
     // end -->
   });
 }
